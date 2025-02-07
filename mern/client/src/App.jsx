a564342ce19f8048f85877807fc5c62ad9cd34c6
ismailmohammed1432 <survivorawais@gmail.com>
2025-02-07 14:03:28 +0530

    Devops

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-8xl">Hello Ismail</h1>
+      <button className="bg-blue-500 text-center text-3xl font-extrabold ">submit</button>
+    </>
+  );
+};
+
+export default App;
